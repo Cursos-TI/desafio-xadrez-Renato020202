645ca629445f6b94e5adc4a50073cd284971d0dd
Renato020202 <almirrenato9@gmail.com>
2025-09-29 01:19:10 +0000

    nivel aventureiro

--- xadrez.c
+++ xadrez.c
@@ -6,7 +6,7 @@ int main() {
 int bispo = 1;
 int torre = 1;
 int rainha = 1;
-
+int cavalomovimento =1;
 
     printf(" Nível Novato - Movimentação das Peças de xadrez\n");
     printf("\n");
@@ -41,7 +41,20 @@ int rainha = 1;
         printf(" esquerda\n");
         rainha++;
     }
-    
+    printf("\n");
+    printf(" a vez do cavalo\n");
+    printf("\n");
+    // introdução do cavalo
+    // loops aninhados 
+
+    while  (  cavalomovimento--  )
+    {
+        for( int cavalo = 1; cavalo <=2; cavalo++)
+        {
+            printf("baixo\n");
+        }
+        printf("esquerda\n");
+    }
 
 
     return 0;
