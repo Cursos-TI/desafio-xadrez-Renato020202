d38154113ded37d3a2d8a549ce3dd51e745c61eb
Renato020202 <almirrenato9@gmail.com>
2025-09-29 15:55:52 +0000

    nivel mestre

--- xadrez.c
+++ xadrez.c
@@ -1,61 +1,117 @@
 #include <stdio.h>
 
+//jogo de xadrez nivel mestre
 
-int main() {
-// declarando a variaveis correspondendo 1 movimento
-int bispo = 1;
-int torre = 1;
-int rainha = 1;
-int cavalomovimento =1;
-
-    printf(" Nível Novato - Movimentação das Peças de xadrez\n");
-    printf("\n");
-    printf(" a vez do bispo\n"); 
-    printf("\n");
-// começara a vez do bispo se movimentando 5 casa a cima/direita
-    do
+// processos de recursividade
+void moverbispo(int passos) {
+    if (passos == 0) 
     {
-        printf("  cima/direita\n");
-        bispo++;
-    } 
-    while  (bispo <= 5 );
+        // condição de parada de movimento
+        printf(" O bispo terminou o movimento...\n");
+        return;
+    }
+        // loop externo
+    for(int vertical = 0; vertical < 1; vertical++)
+     {
+        // loop interno
+        for( int horizontal = 0; horizontal < 1; horizontal++)
+        {
+            printf(" direita/cima\n");
+        }
 
-    printf("\n");
-    printf(" a vez da torre\n");
-    printf("\n");
+    }
+    // chamada recursiva
+     moverbispo(passos - 1);
 
+}
 
-// começara a vez da torre se movimentando 5 casa a direita
-    for(torre; torre <= 5; torre++) 
+void movertorre(int casas){
+if (casas == 0) 
     {
-        printf(" direita\n");
+        // condição de parada de movimento
+        printf(" A torre terminou o movimento...\n");
+        return;
+    }
+
+    if (casas > 0)
+    {
+printf(" direta\n");
+movertorre(casas - 1);// chamada recursiva
     }
 
 
-    printf("\n");
-    printf(" a vez da rainha\n");
-    printf("\n");
-// começara a vez da rainha  se movimentando 8 casa a esquerda
-    while ( rainha <= 8)
-    {
-        printf(" esquerda\n");
-        rainha++;
-    }
-    printf("\n");
-    printf(" a vez do cavalo\n");
-    printf("\n");
-    // introdução do cavalo
-    // loops aninhados 
+}
 
-    while  (  cavalomovimento--  )
+void moverrainha(int casas2){
+
+if (casas2 == 0) 
+    {
+        // condição de parada de movimento
+        printf(" A rainha terminou o movimento...\n");
+        return;
+    }
+
+    if (casas2 > 0)
+    {
+printf(" esquerda\n");
+moverrainha(casas2 - 1);// chamada recursiva
+    }
+}
+
+
+
+// SEGUE A ORDEM DO QUE SERA IMPRIMIDO
+int main (){
+printf("*****JOGO DE XADREZ*****\n  ");
+printf("\n");
+
+
+
+printf("A VEZ DO BISPO!\n");
+printf("\n");
+int bispo = 5;
+moverbispo(bispo);
+
+
+printf("\n");
+printf("A VEZ DA TORRE!\n");
+printf("\n");
+int torre = 5;
+movertorre(torre);
+
+
+printf("\n");
+printf(" A VEZ DA RAINHA!\n");
+printf("\n");
+int rainha = 8;
+moverrainha(rainha);
+
+
+int movercavalo = 1;
+printf("\n");
+printf("A VEZ DO CAVALO!\n");
+printf("\n");
+
+// segue o loop aninhado no cavalo
+while  (  movercavalo--  )
     {
         for( int cavalo = 1; cavalo <=2; cavalo++)
         {
-            printf("baixo\n");
+            printf("cima\n");
         }
-        printf("esquerda\n");
+        printf("direita\n");
+        
     }
+printf("o cavalo finalizou o movimento\n");
+printf("\n");
+
+    
+return 0;
 
 
-    return 0;
 }
+
+ 
+
+
+
